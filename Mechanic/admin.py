7d357510f77c61cbceaba6c6c94308c25713d768
Pratham H Sunnal <126776410+Pratham-H-S@users.noreply.github.com>
2024-02-06 20:21:56 +0530

    Delete Mechanic directory

--- Mechanic/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(UsersMechanic)
-admin.site.register(MechanicDetails)
-admin.site.register(Booking_status)
-admin.site.register(Profile_mechanic)
